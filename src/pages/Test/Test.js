807d3295c71c4b71acdcc5d5d792400ed0d78124
nishankadel <nishankadel39@gmail.com>
2022-08-21 08:49:51 +0545

    updating codes

--- src/pages/Test/Test.js
+++ src/pages/Test/Test.js
@@ -1,11 +1,28 @@
-import React from 'react'
+import axios from "axios";
+import React, { useEffect } from "react";
 
 const Test = () => {
+  const [data, setData] = React.useState([]);
+  useEffect(() => {
+    axios
+      .get("https://dummyjson.com/products")
+      .then((res) => {
+        setData(res.data.products);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
   return (
     <>
-      Test
+      {data.map((product) => (
+        <div key={product.id}>
+          {product.title}
+          {product.price}
+        </div>
+      ))}
     </>
-  )
-}
+  );
+};
 
-export default Test
+export default Test;
