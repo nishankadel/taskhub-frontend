723c4a872421a18af6334ad286404345783ea156
nishankadel <nishankadel39@gmail.com>
2022-08-21 08:28:01 +0545

    updating codes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import Home from "./pages/Home/Home";
+import Test from "./pages/Test/Test";
 
 function App() {
   return (
     <>
-      App
-      <div className="text-green-500">
-        My Name is Kadel
-      </div>
+      <Router>
+        <ToastContainer
+          position="top-right"
+          autoClose={1500}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          theme="dark"
+          pauseOnFocusLoss
+          draggable={false}
+          pauseOnHover={false}
+        />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/test" element={<Test />} />
+        </Routes>
+      </Router>
     </>
   );
 }
